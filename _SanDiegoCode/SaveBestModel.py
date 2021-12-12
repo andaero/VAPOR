c9a3a309bd8807eeb6467a0ef68cebf987835263
andaero <axeman775@gmail.com>
2021-12-11 23:57:24 -0500

    OOP implementation of SaveBestModel

---
+++ _SanDiegoCode/SaveBestModel.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+class SaveBestModel(tf.keras.callbacks.Callback):
+    def __init__(self, save_best_metric='val_root_mean_squared_error'):
+        self.save_best_metric = save_best_metric
+        self.lowestError = 484
+
+    def on_epoch_end(self, epoch, logs=None):
+        # print(logs[self.save_best_metric])
+        if (logs[self.save_best_metric] < self.lowestError):
+            self.lowestError = logs[self.save_best_metric]
+            # self.model.save(f"supply_model/{seq_len}hrinputLiquid-{round(logs[self.save_best_metric],2)}-RMSE.model")
+            self.model.save(
+                f"supply_model_v2/Liquid-{round(logs[self.save_best_metric], 2)}-RMSE.model")
+            print(f"\n------Model with RMSE of {logs[self.save_best_metric]} saved------")
