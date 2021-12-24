23e8ae19e5b03315839a8e6b13619aa39d0c45f9
andaero <axeman775@gmail.com>
2021-12-23 23:23:16 -0500

    Updated to work with OOP VAPORModel.py

--- _SanDiegoCode/Supply_Vapor_CNN_Main.py
+++ _SanDiegoCode/Supply_Vapor_CNN_Main.py
@@ -1,22 +1,14 @@
 import tensorflow as tf
 from Supply_VAPOR_Model_Preprocess import model_preprocess_CNN, model_preprocess
+from VAPORModel import VAPOR_Model as VAPOR
 
-model_preprocess_CNN(12,supplyTotal=False)
 
+train_x_pv, validation_x_pv, train_x_aux, validation_x_aux, train_y, validation_y = model_preprocess_CNN(12,supplyTotal=False, showFig=False)
 
-# model = VAPORModel()
-# model([x_main, x_aux])
-#
-# class VAPORModel(tf.keras.Model):
-#
-#     def __init__(self, name):
-#         self.name = name
-#         self.main = MainModel()
-#         self.aux = SLAM()
-#
-#     def __call__(self, xs):
-#         x_main, x_aux = xs
-#         x = self.aux(x_aux)
-#         ...
-#         x = self.main(x)
-#         return x
+model = VAPOR()
+opt = tf.keras.optimizers.Adam(learning_rate=0.001, decay=1e-6)
+
+model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=opt,
+                      metrics=[tf.keras.metrics.RootMeanSquaredError()])
+model.fit([train_x_pv, train_x_aux], train_y, batch_size=64, epochs=20)
+#validation_data=([validation_x_pv, validation_x_aux], validation_y))
