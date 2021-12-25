2ad4832ecbdb13450ba7e1657effb867935d7bf8
andaero <axeman775@gmail.com>
2021-12-25 16:44:23 -0500

    Changed name to VAPOR_Model

--- _SanDiegoCode/Supply_Vapor_CNN_Main.py
+++ _SanDiegoCode/Supply_Vapor_CNN_Main.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 from Supply_VAPOR_Model_Preprocess import model_preprocess_CNN, model_preprocess
-from VAPORModel import VAPOR_Model as VAPOR
+from VAPORModel import VAPOR_Model
 
 
 train_x_pv, validation_x_pv, train_x_aux, validation_x_aux, train_y, validation_y = model_preprocess_CNN(12,supplyTotal=False, showFig=False)
 
-model = VAPOR()
+model = VAPOR_Model()
 opt = tf.keras.optimizers.Adam(learning_rate=0.001, decay=1e-6)
 
 model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=opt,
