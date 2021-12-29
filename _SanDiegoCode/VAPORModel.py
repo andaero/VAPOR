4826e000a8552c785c791ed5709305fd3a55d0fe
andaero <axeman775@gmail.com>
2021-12-28 21:14:50 -0500

    Updated VAPORModel with RMSE of 1000

--- _SanDiegoCode/VAPORModel.py
+++ _SanDiegoCode/VAPORModel.py
@@ -1,42 +1,59 @@
 import tensorflow as tf
-from SLAM import SLAM
+from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization, MaxPool2D, MaxPool3D, Flatten, RNN, Bidirectional, InputLayer, Conv2D, TimeDistributed, Input, MaxPooling2D, AveragePooling2D
+
+from SLAM_Layer import SLAM_Layer
+from ConvBlock import ConvBlock
 import numpy as np
 
 class VAPOR_Model(tf.keras.Model):
     def __init__(self):
         super(VAPOR_Model, self).__init__()
-        self.aux = SLAM(relu=False)
+        self.slam_1 = SLAM_Layer(relu=False)
+        # self.T_CNN_1 = TimeDistributed(Conv2D(64, (2,2), activation="relu"))
+        self.T_CNN_1 = ConvBlock(64)
+        self.Dropout_1 = Dropout(0.2)
+
+        self.slam_2 = SLAM_Layer(relu=False)
+        self.T_CNN_2 = ConvBlock(128)
+
+        self.slam_3 = SLAM_Layer(relu=False)
+        self.T_CNN_3 = TimeDistributed(Conv2D(128, (2,2), activation="relu"))
+        self.Dropout_2 = Dropout(0.2)
+
+        self.AvgPool2D = TimeDistributed(AveragePooling2D(pool_size=(2,2)))
+        self.T_Flatten = TimeDistributed(Flatten())
+        self.Dense_1 = TimeDistributed(Dense(64, activation="relu"))
+
+        self.LSTM_1 = LSTM(64, return_sequences=True)
+        self.LSTM_2 = LSTM(64, return_sequences=False)
+
+        self.Flatten_LSTM = Flatten()
+        self.Dense_LSTM_1 = Dense(64, activation="linear")
+
+        self.Dense_LSTM_2 = Dense(1, activation="linear")
+
         # self.main = MainModel()
 
     def call(self, inputs):
-        # watch out for batch norm - weird w training=True
         x_main, x_aux = inputs
+        x = self.slam_1(x_main, x_aux)
+        x = self.T_CNN_1(x)
+        x = self.Dropout_1(x)
 
-        aux_slices = tf.unstack(x_aux, axis=1) #axis=1 because 0th dimension is unknown
-        main_slices = tf.unstack(x_main, axis=1) #axis=1 because 0th dimension is unknown
+        # x = self.slam_2(x, x_aux)
+        # x = self.T_CNN_2(x)
 
-        print("AUX SHAPE" , aux_slices)
-        print("MAIN SHAPE" , main_slices)
+        x = self.slam_3(x, x_aux)
+        x = self.T_CNN_3(x)
+        x = self.Dropout_2(x)
+        x = self.AvgPool2D(x)
+        x = self.T_Flatten(x)
+        x = self.Dense_1(x)
+        x = self.LSTM_1(x)
+        # x = self.LSTM_2(x)
 
+        x = self.Flatten_LSTM(x)
+        x = self.Dense_LSTM_1(x)
+        x = self.Dense_LSTM_2(x)
 
-        # c = lambda i : i<len(aux_slices)
-        # x = tf.while_loop(c, self.aux(), aux_slices)
-        i = 0
-        ta_aux = tf.TensorArray(dtype=tf.float32, size=1, dynamic_size=True)
-        ta = tf.TensorArray(dtype=tf.float32, size=1, dynamic_size=True)
-
-        for aux_slice in aux_slices:
-            main_slice_squeezed= tf.squeeze(main_slices[i])
-
-            print("MAIN SLICE SHAPE",main_slice_squeezed.shape)
-
-            ta = ta.write(i, tf.linalg.matmul(main_slice_squeezed,self.aux(aux_slice)))
-            i+=1
-            print(i)
-
-        ta_stacked= ta.stack() # Don't need to stack can just directly multiply by the 10x10 matrices
-        # ta = tf.print(ta, [ta])
-
-        #return a 3x10x10 array
-        print(ta_stacked)
-        return ta_stacked
+        return x
