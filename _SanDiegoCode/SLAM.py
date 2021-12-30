647997853f894a3cc22896eefeba1c5fcf859200
andaero <axeman775@gmail.com>
2021-12-29 21:52:42 -0500

    Updated with SoftMax and 2 dense layers

--- _SanDiegoCode/SLAM.py
+++ _SanDiegoCode/SLAM.py
@@ -7,19 +7,24 @@ from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 
 class SLAM (tf.keras.layers.Layer):
     """Class implementation of Sigmoid Liquid Attention Matrix"""
-    def __init__(self, relu):
+    def __init__(self, dense, relu=False):
         super(SLAM, self).__init__()
         if(relu == False):
-            self.dense = Dense(10, activation="linear")
+            self.dense = Dense(dense, activation="linear")
+            self.dense_2 = Dense(10, activation="linear")
+
         else:
             self.dense = Dense(10, activation="relu")
 
     def call(self, inputs):
         x = self.dense(inputs)
+        x = self.dense_2(x)
+
         # print(x.shape)
         TenByTen = tf.linalg.matmul(x,x, transpose_a=True) #outer product of x
         # print("TEN BY TEN SHAPE", TenByTen.shape)
         SigmoidTenByTen = tf.math.sigmoid(TenByTen)
-        return SigmoidTenByTen
+        SoftMaxTenByTen = tf.math.softmax(TenByTen)
+        return SoftMaxTenByTen
 
 
