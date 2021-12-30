79117083a131c11787b7ea59655d233c12bf9627
andaero <axeman775@gmail.com>
2021-12-29 22:02:08 -0500

    Exclude 10x10 matrix from normalization

--- _SanDiegoCode/Supply_VAPOR_Model_Preprocess.py
+++ _SanDiegoCode/Supply_VAPOR_Model_Preprocess.py
@@ -359,7 +359,8 @@ def model_preprocess_CNN(seq_len, supplyTotal, showFig, normalize):
 
     if normalize==True:
         normalizeList = list(df.columns)
-        normalizeList.remove("target")
+        normalizeList.remove(["target", 'RealPower1', 'RealPower5', 'RealPower8', 'RealPower9', 'RealPower12','RealPower13', 'RealPower16', 'RealPower17', 'RealPower21','RealPower24', 'RealPower25', 'RealPower28', 'RealPower29',
+                              'RealPower33', 'RealPower36', 'RealPower37', 'RealPower40','RealPower41', 'RealPower_42', 'RealPower_43', 'RealPower_44','RealPower_45', 'RealPower_46', 'RealPower_47', 'RealPower_48'])
         df = scaleDataV2(df,normalizeList)
     #PV GEN STUFF
     pvDf = df.drop(["SupplyTotal", "target"], axis=1)
