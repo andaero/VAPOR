a87ede84a40b4948a513f6e92df675520573eac4
andaero <axeman775@gmail.com>
2022-01-17 00:06:46 -0500

    Updated to save weights

--- _SanDiegoCode/SaveBestModel.py
+++ _SanDiegoCode/SaveBestModel.py
@@ -5,13 +5,12 @@ from kerasncp.tf import LTCCell
 class SaveBestModel(tf.keras.callbacks.Callback):
     def __init__(self, save_best_metric='val_root_mean_squared_error'):
         self.save_best_metric = save_best_metric
-        self.lowestError = 470
+        self.lowestError = 640
 
     def on_epoch_end(self, epoch, logs=None):
         # print(logs[self.save_best_metric])
         if (logs[self.save_best_metric] < self.lowestError):
             self.lowestError = logs[self.save_best_metric]
-            # self.model.save(f"supply_model/{seq_len}hrinputLiquid-{round(logs[self.save_best_metric],2)}-RMSE.model")
-            self.model.save(
-                f"supply_model_v2/Liquid-{round(logs[self.save_best_metric], 2)}-RMSE.model")
+            # self.model.save(f"SupplyVAPOR/LiquidNoSLAM-{round(logs[self.save_best_metric], 2)}-RMSE.model")
+            self.model.save_weights(f"SupplyVAPOR/Liquid-{round(logs[self.save_best_metric], 2)}-RMSE")
             print(f"\n------Model with RMSE of {logs[self.save_best_metric]} saved------")
